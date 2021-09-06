5a3428cb07dc35c26eb6ce2c774726dd49b79b93
shozoy11 <10shoh10@gmail.com>
2021-09-06 21:33:40 +0900

    fifth

--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -3,8 +3,8 @@ from .views import signupview, loginview, logoutview, indexview
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
+    path('', loginview, name='login'),
     path('signup/', signupview, name='signup'),
-    path('login/', loginview, name='login'),
     path('logout/', logoutview, name='logout'),
     # path("index/", login_required(indexview), name="index"),
     # path("index/", indexview.as_view(), name="index"),
